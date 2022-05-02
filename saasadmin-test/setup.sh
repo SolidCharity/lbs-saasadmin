c19722b1137f4d6cd6dac40197278d9c3553e43c
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-05-02 17:05:57 +0200

    we need gettext

--- saasadmin-test/setup.sh
+++ saasadmin-test/setup.sh
@@ -5,7 +5,7 @@ if [ ! -z "$1" ]; then
   branch=$1
 fi
 
-apt-get -y install git sqlite || dnf -y install git sqlite || exit -1
+apt-get -y install git sqlite gettext || dnf -y install git sqlite gettext || exit -1
 
 git clone --branch $branch --depth 5 https://github.com/SolidCharity/saasadmin.git
 cd saasadmin
