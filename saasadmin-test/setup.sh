d6ece0254b26d873c89d48bf5ad931f4ac633bc4
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-05-02 19:19:01 +0200

    specific packages for Ubuntu Focal

--- saasadmin-test/setup.sh
+++ saasadmin-test/setup.sh
@@ -5,7 +5,21 @@ if [ ! -z "$1" ]; then
   branch=$1
 fi
 
-apt-get -y install git sqlite gettext python3.8-venv || dnf -y install git sqlite gettext || exit -1
+. /etc/os-release
+OS=$NAME
+VER=$VERSION_ID
+
+if [[ "$OS" == "Ubuntu" || "$OS" == "Debian GNU/Linux" ]]
+then
+    packages="git sqlite gettext"
+    if [[ "$VER" == "20.04" ]]
+    then
+        packages="$packages python3.8-venv"
+    fi
+    apt-get -y install $packages || exit -1
+else
+    dnf -y install git sqlite gettext || exit -1
+fi
 
 git clone --branch $branch --depth 5 https://github.com/SolidCharity/saasadmin.git
 cd saasadmin
