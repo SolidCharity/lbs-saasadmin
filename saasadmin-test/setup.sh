190d4a7418cb8a36dc257d0e70eef84ad040c6c7
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-05-02 14:42:28 +0200

    need git

--- saasadmin-test/setup.sh
+++ saasadmin-test/setup.sh
@@ -5,6 +5,8 @@ if [ ! -z "$1" ]; then
   branch=$1
 fi
 
+apt-get install git || dnf install git || exit -1
+
 git clone --branch $branch --depth 5 https://github.com/SolidCharity/saasadmin.git
 cd saasadmin
 make quickstart || exit -1
