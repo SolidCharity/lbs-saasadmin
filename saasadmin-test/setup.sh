91e187135b213b611cb90d137bd207beb8854f0b
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-05-03 09:09:04 +0200

    use python 3.9 for CentOS Stream

--- saasadmin-test/setup.sh
+++ saasadmin-test/setup.sh
@@ -14,13 +14,22 @@ then
     packages="git sqlite3 gettext python3-venv"
     apt-get -y install $packages || exit -1
 else
-    packages="git sqlite gettext python3"
+    packages="git sqlite gettext"
     dnf -y install $packages || exit -1
+
+    if [[ "$VER" == "9" ]]; then
+        yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm || exit -1
+    else
+        yum -y install epel-release || exit -1
+    fi
+    if [[ "$VER" == "8" || "$VER" == "9" ]]; then
+        yum -y install python39-devel || exit -1
+        alternatives --set python3 /usr/bin/python3.9 || exit -1
+    fi
 fi
 
 git clone --branch $branch --depth 5 https://github.com/SolidCharity/saasadmin.git
 cd saasadmin
-export LANG=en_US.UTF-8
 make quickstart || exit -1
 make collectstatic || exit -1
 make messages || exit -1
